79793df65d8ea6a15bfe582fadf48229d8cb5894
Tim Riemer <tim.riemer@vorwerk.de>
2018-09-04 13:24:42 +0200

    add Spring Boot related properties to use JUnit 5.3 and JUnit Platform 1.3.0
    activate all Spring related annotations

--- src/main/java/mobi/riemer/junit5_springboot_test/Junit5SpringbootTestApplication.java
+++ src/main/java/mobi/riemer/junit5_springboot_test/Junit5SpringbootTestApplication.java
@@ -1,9 +1,12 @@
 package mobi.riemer.junit5_springboot_test;
 
-//@SpringBootApplication
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Junit5SpringbootTestApplication {
 
-//    public static void main(String[] args) {
-//        SpringApplication.run(Junit5SpringbootTestApplication.class, args);
-//    }
+    public static void main(String[] args) {
+        SpringApplication.run(Junit5SpringbootTestApplication.class, args);
+    }
 }
--- src/test/java/mobi/riemer/junit5_springboot_test/Junit5SpringbootTestApplicationTests.java
+++ src/test/java/mobi/riemer/junit5_springboot_test/Junit5SpringbootTestApplicationTests.java
@@ -1,9 +1,12 @@
 package mobi.riemer.junit5_springboot_test;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class Junit5SpringbootTestApplicationTests {
 
     @Test
