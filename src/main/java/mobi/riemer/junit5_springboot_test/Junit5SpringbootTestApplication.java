e8aa5a8b3a148534635fe539f83788863a942970
Tim Riemer <tim.riemer@vorwerk.de>
2018-09-04 11:54:23 +0200

    Initial commit

---
+++ src/main/java/mobi/riemer/junit5_springboot_test/Junit5SpringbootTestApplication.java
@@ -0,0 +1,9 @@
+package mobi.riemer.junit5_springboot_test;
+
+//@SpringBootApplication
+public class Junit5SpringbootTestApplication {
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(Junit5SpringbootTestApplication.class, args);
+//    }
+}
---
+++ src/test/java/mobi/riemer/junit5_springboot_test/Junit5SpringbootTestApplicationTests.java
@@ -0,0 +1,14 @@
+package mobi.riemer.junit5_springboot_test;
+
+import org.junit.jupiter.api.Test;
+
+//@ExtendWith(SpringExtension.class)
+//@SpringBootTest
+class Junit5SpringbootTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("TEST");
+    }
+
+}
